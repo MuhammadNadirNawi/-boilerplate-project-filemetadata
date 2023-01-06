05e4c7dd94f96ad1857c5e7a1a2dba742c0a6410
Muhammad Nadir Nawi <zakirdev2002@gmail.com>
2023-01-06 11:03:11 +0800

    add route /api/fileanalyse

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const fileUpload = require('express-fileupload');
+
 
 var app = express();
-
+app.use(fileUpload());
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -11,6 +13,14 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse", (req, res) => {
+  if (req.files === null) {
+    return res.status(400).json({ msg: 'No file was uploaded' });
+  }
+  const file = req.files.upfile;
+
+  res.json({ name: file.name, type: file.mimetype, size: file.size });
+})
 
 
 
